45dac8e87284e0b6da94abdfd174c67caaffc774
YunaiV <>
2019-11-13 09:29:10 +0800

    增加数据库版本管理示例（flyway）

---
+++ lab-20/lab-20-database-version-control-flyway/src/main/resources/db/migration/V1.0__INIT_DB.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户编号',
+  `username` varchar(64) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '账号',
+  `password` varchar(32) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '密码',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
