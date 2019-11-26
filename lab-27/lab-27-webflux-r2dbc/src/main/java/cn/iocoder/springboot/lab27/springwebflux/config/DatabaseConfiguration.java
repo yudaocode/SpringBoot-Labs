5000b79a0a695dec28db85929e200796c1885b6f
YunaiV <>
2019-11-27 00:16:43 +0800

    增加 webflux mysql + 事务

--- lab-27/lab-27-webflux-r2dbc/src/main/java/cn/iocoder/springboot/lab27/springwebflux/config/DatabaseConfiguration.java
+++ lab-27/lab-27-webflux-r2dbc/src/main/java/cn/iocoder/springboot/lab27/springwebflux/config/DatabaseConfiguration.java
@@ -6,13 +6,15 @@ import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.connectionfactory.R2dbcTransactionManager;
+import org.springframework.transaction.ReactiveTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
 @Configuration
-@EnableTransactionManagement
+@EnableTransactionManagement // 开启事务的支持
 public class DatabaseConfiguration {
 
     @Bean
@@ -27,6 +29,11 @@ public class DatabaseConfiguration {
                 properties.getUsername(), host, port, properties.getPassword(), database);
         // 创建 JasyncConnectionFactory 对象
         return  new JasyncConnectionFactory(new MySQLConnectionFactory(configuration));
+    }
+
+    @Bean
+    public ReactiveTransactionManager transactionManager(R2dbcProperties properties) throws URISyntaxException {
+        return new R2dbcTransactionManager(this.connectionFactory(properties));
     }
 
 }
