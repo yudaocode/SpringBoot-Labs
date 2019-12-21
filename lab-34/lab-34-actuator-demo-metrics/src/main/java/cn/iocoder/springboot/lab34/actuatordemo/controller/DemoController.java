6ca4a5cc88711a8e51a7eec96cd8a0f3ad70da6e
YunaiV <>
2019-12-22 00:35:56 +0800

    增加 actuate metrics 示例

--- lab-34/lab-34-actuator-demo-metrics/src/main/java/cn/iocoder/springboot/lab34/actuatordemo/controller/DemoController.java
+++ lab-34/lab-34-actuator-demo-metrics/src/main/java/cn/iocoder/springboot/lab34/actuatordemo/controller/DemoController.java
@@ -13,16 +13,13 @@ public class DemoController {
     /**
      * DEMO 访问次数 Metrics
      */
-//    private static final Counter METRICS_DEMO_COUNT = Metrics.counter("demo.visit.count");
-    /**
-     * DEMO 访问次数 Metrics
-     */
     private static final Counter METRICS_DEMO_COUNT = Counter
             .builder("demo.visit.count") // 指标的名字
             .description("demo 访问次数") // 指标的描述
             .baseUnit("次") // 指标的单位
             .tag("test", "nicai") // 自定义标签
             .register(Metrics.globalRegistry); // 注册到全局 MeterRegistry 指标注册表
+//    private static final Counter METRICS_DEMO_COUNT = Metrics.counter("demo.visit.count");
 
     @GetMapping("/visit")
     public String visit() {
--- lab-34/lab-34-actuator-demo-metrics/src/main/java/cn/iocoder/springboot/lab34/actuatordemo/controller/ExampleController.java
+++ lab-34/lab-34-actuator-demo-metrics/src/main/java/cn/iocoder/springboot/lab34/actuatordemo/controller/ExampleController.java
@@ -1,19 +1,20 @@
 package cn.iocoder.springboot.lab34.actuatordemo.controller;
 
-import io.micrometer.core.annotation.Counted;
 import io.micrometer.core.annotation.Timed;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// TODO 暂时没生效 https://docs.spring.io/spring-boot/docs/current/reference/html/production-ready-features.html#production-ready-metrics-spring-mvc
+// 参考 https://docs.spring.io/spring-boot/docs/current/reference/html/production-ready-features.html#production-ready-metrics-spring-mvc 文档
+// 需要设置 management.metrics.web.server.request.autotime.enabled = false
 @RestController
 @RequestMapping("/example")
 @Timed
 public class ExampleController {
 
     @GetMapping("/visit")
-    @Counted(value = "example.visit.count", description = "example 访问次数")
+//    @Counted(value = "example.visit.count", description = "example 访问次数"
+    @Timed(value = "all.people", longTask = true)
     public String visit() {
         return "Example 示例";
     }
