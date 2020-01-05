c7aa85d99375a4e059155a192187829bd259aaf0
YunaiV <>
2020-01-05 23:19:36 +0800

    增加 skywalking 示例 - opentracing

---
+++ lab-39/lab-39-opentracing/src/main/java/cn/iocoder/springboot/lab39/skywalkingdemo/OpentracingApplication.java
@@ -0,0 +1,13 @@
+package cn.iocoder.springboot.lab39.skywalkingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentracingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpentracingApplication.class, args);
+    }
+
+}
