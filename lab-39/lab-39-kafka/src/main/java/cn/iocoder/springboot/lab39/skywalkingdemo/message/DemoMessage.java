b32d39f9af6db5784ec3156b3bc882dccae3ffee
YunaiV <>
2020-01-05 10:07:02 +0800

    增加 skywalking 示例 - kafka

---
+++ lab-39/lab-39-kafka/src/main/java/cn/iocoder/springboot/lab39/skywalkingdemo/message/DemoMessage.java
@@ -0,0 +1,31 @@
+package cn.iocoder.springboot.lab39.skywalkingdemo.message;
+
+/**
+ * 示例 Message 消息
+ */
+public class DemoMessage {
+
+    public static final String TOPIC = "DEMO";
+
+    /**
+     * 编号
+     */
+    private Integer id;
+
+    public DemoMessage setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "DemoMessage{" +
+                "id=" + id +
+                '}';
+    }
+
+}
