5f67e5c312c8b9ad58f660432224abf1d72639ac
YunaiV <>
2020-01-19 22:12:39 +0800

    增加配置文件示例

---
+++ lab-43/lab-43-demo-profiles/src/main/java/cn/iocoder/springboot/lab43/propertydemo/ProfilesApplication.java
@@ -0,0 +1,13 @@
+package cn.iocoder.springboot.lab43.propertydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfilesApplication.class, args);
+    }
+
+}
