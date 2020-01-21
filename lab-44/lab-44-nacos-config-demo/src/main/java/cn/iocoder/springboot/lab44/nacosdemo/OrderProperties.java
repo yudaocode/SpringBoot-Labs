5f5e8a259fe1a81de62e7ec6ebad814a07b29710
YunaiV <>
2020-01-21 19:50:35 +0800

    nacos 示例

---
+++ lab-44/lab-44-nacos-config-demo/src/main/java/cn/iocoder/springboot/lab44/nacosdemo/OrderProperties.java
@@ -0,0 +1,53 @@
+package cn.iocoder.springboot.lab44.nacosdemo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+//@NacosConfigurationProperties(prefix = "order", dataId = "${nacos.config.data-id}", type = ConfigType.YAML)
+@ConfigurationProperties(prefix = "order")
+public class OrderProperties {
+
+    /**
+     * 订单支付超时时长，单位：秒。
+     */
+    private Integer payTimeoutSeconds;
+
+    /**
+     * 订单创建频率，单位：秒
+     */
+    private Integer createFrequencySeconds;
+
+//    /**
+//     * 配置描述
+//     */
+//    private String desc;
+
+    public Integer getPayTimeoutSeconds() {
+        return payTimeoutSeconds;
+    }
+
+    public OrderProperties setPayTimeoutSeconds(Integer payTimeoutSeconds) {
+        this.payTimeoutSeconds = payTimeoutSeconds;
+        return this;
+    }
+
+    public Integer getCreateFrequencySeconds() {
+        return createFrequencySeconds;
+    }
+
+    public OrderProperties setCreateFrequencySeconds(Integer createFrequencySeconds) {
+        this.createFrequencySeconds = createFrequencySeconds;
+        return this;
+    }
+
+//    public String getDesc() {
+//        return desc;
+//    }
+//
+//    public OrderProperties setDesc(String desc) {
+//        this.desc = desc;
+//        return this;
+//    }
+
+}
