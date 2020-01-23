7d5659b3d13b3100f750d6eeb1e8c5bb0ef9270c
YunaiV <>
2020-01-23 14:45:11 +0800

    nacos 动态刷新示例

--- lab-44/lab-44-nacos-config-demo-auto-refresh/src/main/java/cn/iocoder/springboot/lab44/nacosdemo/listener/LoggingSystemConfigListener.java
+++ lab-44/lab-44-nacos-config-demo-auto-refresh/src/main/java/cn/iocoder/springboot/lab44/nacosdemo/listener/LoggingSystemConfigListener.java
@@ -13,6 +13,9 @@ import java.util.Properties;
 @Component
 public class LoggingSystemConfigListener {
 
+    /**
+     * 日志配置项的前缀
+     */
     private static final String LOGGER_TAG = "logging.level.";
 
     @Resource
@@ -20,12 +23,17 @@ public class LoggingSystemConfigListener {
 
     @NacosConfigListener(dataId = "${nacos.config.data-id}", type = ConfigType.YAML, timeout = 5000)
     public void onChange(String newLog) throws Exception {
+        // 使用 DefaultYamlConfigParse 工具类，解析配置
         Properties properties = new DefaultYamlConfigParse().parse(newLog);
+        // 遍历配置集的每个配置项，判断是否是 logging.level 配置项
         for (Object t : properties.keySet()) {
             String key = String.valueOf(t);
+            // 如果是 logging.level 配置项，则设置其对应的日志级别
             if (key.startsWith(LOGGER_TAG)) {
+                // 获得日志级别
                 String strLevel = properties.getProperty(key, "info");
                 LogLevel level = LogLevel.valueOf(strLevel.toUpperCase());
+                // 设置日志级别到 LoggingSystem 中
                 loggingSystem.setLogLevel(key.replace(LOGGER_TAG, ""), level);
             }
         }
