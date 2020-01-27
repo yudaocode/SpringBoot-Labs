b94f5cf9a65bc50a57d10c8d8870d968698c3a79
YunaiV <>
2020-01-27 20:49:13 +0800

    Apollo 配置加载顺序示例

--- lab-45/lab-45-apollo-demo-jasypt/src/test/java/cn/iocoder/springboot/lab45/apollodemo/JasyptTest.java
+++ lab-45/lab-45-apollo-demo-jasypt/src/test/java/cn/iocoder/springboot/lab45/apollodemo/JasyptTest.java
@@ -18,16 +18,15 @@ public class JasyptTest {
     @Test
     public void encode() {
         // 第一个加密
-//        String applicationName = "demo-application";
-//        System.out.println(encryptor.encrypt(applicationName));
+        String applicationName = "demo-application";
+        System.out.println(encryptor.encrypt(applicationName));
 
 //        // 第二个加密
-        applicationName = "demo-app";
-        System.out.println(encryptor.encrypt(applicationName));
+//        applicationName = "demo-app";
+//        System.out.println(encryptor.encrypt(applicationName));
     }
 
     @Value("${spring.application.name}")
-//    @NacosValue("${spring.application.name}")
     private String applicationName;
 
     @Test
