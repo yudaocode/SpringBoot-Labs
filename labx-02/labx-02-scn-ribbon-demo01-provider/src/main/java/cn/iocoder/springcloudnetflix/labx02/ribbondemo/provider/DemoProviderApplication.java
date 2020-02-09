8f9484103df1a84a51f9057e647d544a6d1bda56
YunaiV <>
2020-02-09 12:35:17 +0800

    增加 spring cloud netflix ribbon 入门示例

--- labx-02/labx-02-scn-ribbon-demo01-provider/src/main/java/cn/iocoder/springcloudnetflix/labx02/ribbondemo/provider/DemoProviderApplication.java
+++ labx-02/labx-02-scn-ribbon-demo01-provider/src/main/java/cn/iocoder/springcloudnetflix/labx02/ribbondemo/provider/DemoProviderApplication.java
@@ -1,9 +1,10 @@
 package cn.iocoder.springcloudnetflix.labx02.ribbondemo.provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +18,19 @@ public class DemoProviderApplication {
     @RestController
     static class TestController {
 
+        private Logger logger = LoggerFactory.getLogger(TestController.class);
+
         @Value("${server.port}")
         private Integer serverPort;
 
         @GetMapping("/echo")
-        public String echo(String name) {
+        public String echo(String name) throws InterruptedException {
+            // 模拟执行 100ms 时长。方便后续我们测试请求超时
+            Thread.sleep(100L);
+
+            // 记录被调用的日志
+            logger.info("[echo][被调用啦 name({})]", name);
+
             return serverPort + "-provider:" + name;
         }
 
