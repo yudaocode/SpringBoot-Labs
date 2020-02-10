44903f519738fc7b54d826bae20fae51146f650b
YunaiV <>
2020-02-10 23:33:01 +0800

    增加 spring cloud openfeign 入门示例

--- labx-03/labx-03-sc-feign-demo02B-consumer/src/main/java/cn/iocoder/springcloud/labx03/feigndemo/consumer/config/DefaultFeignClientConfiguration.java
+++ labx-03/labx-03-sc-feign-demo02B-consumer/src/main/java/cn/iocoder/springcloud/labx03/feigndemo/consumer/config/DefaultFeignClientConfiguration.java
@@ -3,6 +3,9 @@ package cn.iocoder.springcloud.labx03.feigndemo.consumer.config;
 import feign.Logger;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * 全局 FeignClient 配置类
+ */
 public class DefaultFeignClientConfiguration {
 
     @Bean
--- labx-03/labx-03-sc-feign-demo02B-consumer/src/main/java/cn/iocoder/springcloud/labx03/feigndemo/consumer/config/DemoProviderFeignClientConfiguration.java
+++ labx-03/labx-03-sc-feign-demo02B-consumer/src/main/java/cn/iocoder/springcloud/labx03/feigndemo/consumer/config/DemoProviderFeignClientConfiguration.java
@@ -4,12 +4,15 @@ import feign.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
+/**
+ * 服务 demo-provider 的 FeignClient 配置类
+ */
 public class DemoProviderFeignClientConfiguration {
 
     @Bean
-    @Primary
+    @Primary // 主 Bean
     public Logger.Level feignClientLoggerLevel() {
-        return Logger.Level.BASIC;
+        return Logger.Level.FULL;
     }
 
 }
