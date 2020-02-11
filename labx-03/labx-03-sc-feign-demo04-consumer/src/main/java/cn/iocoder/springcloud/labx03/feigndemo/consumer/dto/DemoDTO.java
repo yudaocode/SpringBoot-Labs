5caa5d343459c1064bd7f680a9ee55169e7741c5
YunaiV <>
2020-02-12 00:12:29 +0800

    增加 spring cloud openfeign 多参数示例

---
+++ labx-03/labx-03-sc-feign-demo04-consumer/src/main/java/cn/iocoder/springcloud/labx03/feigndemo/consumer/dto/DemoDTO.java
@@ -0,0 +1,26 @@
+package cn.iocoder.springcloud.labx03.feigndemo.consumer.dto;
+
+public class DemoDTO {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public DemoDTO setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public DemoDTO setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+}
