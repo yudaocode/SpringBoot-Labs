c85ca25a17434d1099efef5db3445b5d5f1eb3d9
YunaiV <>
2020-02-11 23:16:38 +0800

    增加 spring cloud openfeign 入门示例

--- labx-03/labx-03-sc-feign-demo01-consumer/src/main/java/cn/iocoder/springcloud/labx03/feigndemo/consumer/conffig/OpenFeignConfiguration.java
+++
@@ -1,8 +0,0 @@
-package cn.iocoder.springcloud.labx03.feigndemo.consumer.conffig;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class OpenFeignConfiguration {
-
-}
--- labx-03/labx-03-sc-feign-demo02B-consumer/src/main/java/cn/iocoder/springcloud/labx03/feigndemo/consumer/DemoConsumerApplication.java
+++ labx-03/labx-03-sc-feign-demo02B-consumer/src/main/java/cn/iocoder/springcloud/labx03/feigndemo/consumer/DemoConsumerApplication.java
@@ -1,42 +1,16 @@
 package cn.iocoder.springcloud.labx03.feigndemo.consumer;
 
 import cn.iocoder.springcloud.labx03.feigndemo.consumer.config.DefaultFeignClientConfiguration;
-import cn.iocoder.springcloud.labx03.feigndemo.consumer.feign.DemoProviderFeignClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableFeignClients(defaultConfiguration = DefaultFeignClientConfiguration.class)
 public class DemoConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoConsumerApplication.class, args);
-    }
-
-    @Configuration
-    @EnableFeignClients(defaultConfiguration = DefaultFeignClientConfiguration.class)
-    public class OpenFeignConfiguration {
-
-    }
-
-    @RestController
-    static class TestController {
-
-        @Autowired
-        private DemoProviderFeignClient demoProviderFeignClient;
-
-        @GetMapping("/hello02")
-        public String hello02(String name) {
-            // 使用 Feign 调用接口
-            String response = demoProviderFeignClient.echo(name);
-            // 返回结果
-            return "consumer:" + response;
-        }
-
     }
 
 }
