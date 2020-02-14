cda3f3cb762d79a05c16b4408c4fcdffacec8703
YunaiV <>
2020-02-14 21:28:17 +0800

    增加 spring cloud alibaba sentinel 服务容错

--- labx-04/labx-04-sca-sentinel-demo01-provider/src/main/java/cn/iocoder/springcloudalibaba/labx04/sentineldemo/provider/controller/DemoController.java
+++ labx-04/labx-04-sca-sentinel-demo01-provider/src/main/java/cn/iocoder/springcloudalibaba/labx04/sentineldemo/provider/controller/DemoController.java
@@ -1,5 +1,6 @@
 package cn.iocoder.springcloudalibaba.labx04.sentineldemo.provider.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public class DemoController {
 
+    // 测试【流量控制】
     @GetMapping("/echo")
     public String echo() {
         return "echo";
@@ -16,6 +18,20 @@ public class DemoController {
     @GetMapping("/test")
     public String test() {
         return "test";
+    }
+
+    // 测试【熔断降级】
+    @GetMapping("/sleep")
+    public String sleep() throws InterruptedException {
+        Thread.sleep(100L);
+        return "sleep";
+    }
+
+    // 测试【热点参数限流】
+    @GetMapping("/product_info")
+    @SentinelResource("demo_product_info_hot")
+    public String productInfo(Integer id) {
+        return "商品编号：" + id;
     }
 
 }
