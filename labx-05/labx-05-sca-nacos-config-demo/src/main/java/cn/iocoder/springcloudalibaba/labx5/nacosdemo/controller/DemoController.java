5f9ffb67e4ccaf6212840b432cc6e5cb96541eb7
YunaiV <>
2020-02-16 21:33:37 +0800

    增加 spring cloud alibaba nacos 配置中心

--- labx-05/labx-05-sca-nacos-config-demo/src/main/java/cn/iocoder/springcloudalibaba/labx5/nacosdemo/controller/DemoController.java
+++ labx-05/labx-05-sca-nacos-config-demo/src/main/java/cn/iocoder/springcloudalibaba/labx5/nacosdemo/controller/DemoController.java
@@ -17,21 +17,22 @@ public class DemoController {
     @Autowired
     private OrderProperties orderProperties;
 
-    // 测试 @ConfigurationProperties 注解的配置属性类
+    /**
+     * 测试 @ConfigurationProperties 注解的配置属性类
+     */
     @GetMapping("/test01")
     public OrderProperties test01() {
         return orderProperties;
     }
 
-    // @NacosValue(value = "${order.pay-timeout-seconds}")
-    @Value(value = "${order.pay-timeout-seconds}")
+    @Value(value = "${order.pay-timeout-seconds}") // @NacosValue(value = "${order.pay-timeout-seconds}")
     private Integer payTimeoutSeconds;
-
-    // @NacosValue(value = "${order.create-frequency-seconds}")
-    @Value(value = "${order.create-frequency-seconds}")
+    @Value(value = "${order.create-frequency-seconds}") // @NacosValue(value = "${order.create-frequency-seconds}")
     private Integer createFrequencySeconds;
 
-    // 测试 @Value 注解的属性
+    /**
+     * 测试 @Value 注解的属性
+     */
     @GetMapping("/test02")
     public Map<String, Object> test02() {
         return new JSONObject().fluentPut("payTimeoutSeconds", payTimeoutSeconds)
