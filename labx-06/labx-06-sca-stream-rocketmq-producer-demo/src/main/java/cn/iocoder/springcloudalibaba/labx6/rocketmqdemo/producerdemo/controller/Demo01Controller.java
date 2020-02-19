e9b0115094a3a1a824f9f68e46351daab0f469ea
YunaiV <>
2020-02-19 21:38:53 +0800

    增加 spring cloud alibaba rocketmq 消息队列的示例

--- labx-06/labx-06-sca-stream-rocketmq-producer-demo/src/main/java/cn/iocoder/springcloudalibaba/labx6/rocketmqdemo/producerdemo/controller/Demo01Controller.java
+++ labx-06/labx-06-sca-stream-rocketmq-producer-demo/src/main/java/cn/iocoder/springcloudalibaba/labx6/rocketmqdemo/producerdemo/controller/Demo01Controller.java
@@ -2,6 +2,9 @@ package cn.iocoder.springcloudalibaba.labx6.rocketmqdemo.producerdemo.controller
 
 import cn.iocoder.springcloudalibaba.labx6.rocketmqdemo.producerdemo.message.Demo01Message;
 import cn.iocoder.springcloudalibaba.labx6.rocketmqdemo.producerdemo.message.MySource;
+import org.apache.rocketmq.common.message.MessageConst;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
@@ -15,6 +18,8 @@ import java.util.Random;
 @RequestMapping("/demo01")
 public class Demo01Controller {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @Autowired
     private MySource mySource;
 
@@ -23,11 +28,26 @@ public class Demo01Controller {
         // 创建 Message
         Demo01Message message = new Demo01Message()
                 .setId(new Random().nextInt());
-        // 创建
+        // 创建 Spring Message 对象
         Message<Demo01Message> springMessage = MessageBuilder.withPayload(message)
                 .build();
         // 发送消息
         return mySource.demo01Output().send(springMessage);
+    }
+
+    @GetMapping("/send_delay")
+    public boolean sendDelay() {
+        // 创建 Message
+        Demo01Message message = new Demo01Message()
+                .setId(new Random().nextInt());
+        // 创建 Spring Message 对象
+        Message<Demo01Message> springMessage = MessageBuilder.withPayload(message)
+                .setHeader(MessageConst.PROPERTY_DELAY_TIME_LEVEL, "3") // 设置延迟级别为 3，10 秒后消费。
+                .build();
+        // 发送消息
+        boolean sendResult = mySource.demo01Output().send(springMessage);
+        logger.info("[sendDelay][发送消息完成, 结果 = {}]", sendResult);
+        return sendResult;
     }
 
 }
