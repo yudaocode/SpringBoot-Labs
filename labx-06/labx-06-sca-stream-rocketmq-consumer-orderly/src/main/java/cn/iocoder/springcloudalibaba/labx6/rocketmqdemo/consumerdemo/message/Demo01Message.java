986aad8c18df08b48426d61d10b653db75d70bd9
YunaiV <>
2020-02-20 02:09:20 +0800

    增加 spring cloud alibaba rocketmq 消息队列的示例

---
+++ labx-06/labx-06-sca-stream-rocketmq-consumer-orderly/src/main/java/cn/iocoder/springcloudalibaba/labx6/rocketmqdemo/consumerdemo/message/Demo01Message.java
@@ -0,0 +1,29 @@
+package cn.iocoder.springcloudalibaba.labx6.rocketmqdemo.consumerdemo.message;
+
+/**
+ * 示例 01 的 Message 消息
+ */
+public class Demo01Message {
+
+    /**
+     * 编号
+     */
+    private Integer id;
+
+    public Demo01Message setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Demo01Message{" +
+                "id=" + id +
+                '}';
+    }
+
+}
