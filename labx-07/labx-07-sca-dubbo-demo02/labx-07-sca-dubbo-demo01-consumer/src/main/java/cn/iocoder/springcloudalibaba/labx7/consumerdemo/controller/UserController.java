0eb518e9adc8441ca085b840b157b6b5059c757a
YunaiV <>
2020-02-22 12:39:56 +0800

    增加 spring cloud alibaba dubbo 服务调用的示例

---
+++ labx-07/labx-07-sca-dubbo-demo02/labx-07-sca-dubbo-demo01-consumer/src/main/java/cn/iocoder/springcloudalibaba/labx7/consumerdemo/controller/UserController.java
@@ -0,0 +1,23 @@
+package cn.iocoder.springcloudalibaba.labx7.consumerdemo.controller;
+
+import cn.iocoder.springcloudalibaba.labx7.api.UserService;
+import cn.iocoder.springcloudalibaba.labx7.dto.UserDTO;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class UserController {
+
+    @Reference(version = "1.0.0")
+    private UserService userService;
+
+    @GetMapping("/get")
+    public UserDTO get(@RequestParam("id") Integer id) {
+        return userService.get(id);
+    }
+
+}
---
+++ labx-07/labx-07-sca-dubbo-demo02/labx-07-sca-dubbo-demo02-provider-springmvc/src/main/java/cn/iocoder/springcloudalibaba/labx7/providerdemo/ProviderApplication.java
@@ -0,0 +1,13 @@
+package cn.iocoder.springcloudalibaba.labx7.providerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class);
+    }
+
+}
