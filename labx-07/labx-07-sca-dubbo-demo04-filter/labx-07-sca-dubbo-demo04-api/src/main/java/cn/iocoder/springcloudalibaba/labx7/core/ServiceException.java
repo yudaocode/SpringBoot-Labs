446f7f8e6d01bd803d71ab26049253dcfc999905
YunaiV <>
2020-02-23 22:04:15 +0800

    增加 spring cloud alibaba dubbo 服务调用的示例，自定义 spi

---
+++ labx-07/labx-07-sca-dubbo-demo04-filter/labx-07-sca-dubbo-demo04-api/src/main/java/cn/iocoder/springcloudalibaba/labx7/core/ServiceException.java
@@ -0,0 +1,58 @@
+package cn.iocoder.springcloudalibaba.labx7.core;
+
+/**
+ * 服务异常
+ *
+ * 参考 https://www.kancloud.cn/onebase/ob/484204 文章
+ *
+ * 一共 10 位，分成四段
+ *
+ * 第一段，1 位，类型
+ *      1 - 业务级别异常
+ *      2 - 系统级别异常
+ * 第二段，3 位，系统类型
+ *      001 - 用户系统
+ *      002 - 商品系统
+ *      003 - 订单系统
+ *      004 - 支付系统
+ *      005 - 优惠劵系统
+ *      ... - ...
+ * 第三段，3 位，模块
+ *      不限制规则。
+ *      一般建议，每个系统里面，可能有多个模块，可以再去做分段。以用户系统为例子：
+ *          001 - OAuth2 模块
+ *          002 - User 模块
+ *          003 - MobileCode 模块
+ * 第四段，3 位，错误码
+ *       不限制规则。
+ *       一般建议，每个模块自增。
+ */
+public final class ServiceException extends RuntimeException {
+
+    /**
+     * 错误码
+     */
+    private Integer code;
+
+    public ServiceException() { // 创建默认构造方法，用于反序列化的场景。
+    }
+
+    public ServiceException(ServiceExceptionEnum serviceExceptionEnum) {
+        // 使用父类的 message 字段
+        super(serviceExceptionEnum.getMessage());
+        // 设置错误码
+        this.code = serviceExceptionEnum.getCode();
+    }
+
+    public ServiceException(ServiceExceptionEnum serviceExceptionEnum, String message) {
+        // 使用父类的 message 字段
+        super(message);
+        // 设置错误码
+        this.code = serviceExceptionEnum.getCode();
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+}
---
+++ labx-07/labx-07-sca-dubbo-demo04-filter/labx-07-sca-dubbo-demo04-consumer/src/main/java/cn/iocoder/springcloudalibaba/labx7/consumerdemo/controller/UserController.java
@@ -0,0 +1,26 @@
+package cn.iocoder.springcloudalibaba.labx7.consumerdemo.controller;
+
+import cn.iocoder.springcloudalibaba.labx7.api.UserService;
+import cn.iocoder.springcloudalibaba.labx7.dto.UserAddDTO;
+import cn.iocoder.springcloudalibaba.labx7.dto.UserDTO;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Reference(protocol = "dubbo", version = "1.0.0", validation = "true")
+    private UserService userService;
+
+    @GetMapping("/get")
+    public UserDTO get(@RequestParam("id") Integer id) {
+        return userService.get(id);
+    }
+
+    @PostMapping("/add")
+    public Integer add(UserAddDTO addDTO) {
+        return userService.add(addDTO);
+    }
+
+}
