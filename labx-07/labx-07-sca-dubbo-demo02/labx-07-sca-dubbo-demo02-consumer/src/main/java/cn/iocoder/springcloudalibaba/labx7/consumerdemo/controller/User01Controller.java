4be48ae01e677c98350bd719cb99da50fe989f8d
YunaiV <>
2020-02-23 09:53:37 +0800

    增加 spring cloud alibaba dubbo 服务调用的示例，项目结构改造

--- labx-07/labx-07-sca-dubbo-demo02/labx-07-sca-dubbo-demo02-consumer/src/main/java/cn/iocoder/springcloudalibaba/labx7/consumerdemo/controller/User01Controller.java
+++ labx-07/labx-07-sca-dubbo-demo02/labx-07-sca-dubbo-demo02-consumer/src/main/java/cn/iocoder/springcloudalibaba/labx7/consumerdemo/controller/User01Controller.java
@@ -1,8 +1,8 @@
 package cn.iocoder.springcloudalibaba.labx7.consumerdemo.controller;
 
-import cn.iocoder.springcloudalibaba.labx7.api.UserService;
+import cn.iocoder.springcloudalibaba.labx7.consumerdemo.feign.UserFeignClient;
 import cn.iocoder.springcloudalibaba.labx7.dto.UserDTO;
-import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user01")
 public class User01Controller {
 
-    @Reference(version = "1.0.0", protocol = "dubbo")
-    private UserService userService;
+    @Autowired
+    private UserFeignClient userFeignClient;
 
     @GetMapping("/get")
     public UserDTO get(@RequestParam("id") Integer id) {
-        return userService.get(id);
+        return userFeignClient.get(id);
     }
 
 }
--- labx-07/labx-07-sca-dubbo-demo02/labx-07-sca-dubbo-demo02-consumer/src/main/java/cn/iocoder/springcloudalibaba/labx7/consumerdemo/controller/User04Controller.java
+++ labx-07/labx-07-sca-dubbo-demo02/labx-07-sca-dubbo-demo02-consumer/src/main/java/cn/iocoder/springcloudalibaba/labx7/consumerdemo/controller/User04Controller.java
@@ -1,23 +1,23 @@
 package cn.iocoder.springcloudalibaba.labx7.consumerdemo.controller;
 
-import cn.iocoder.springcloudalibaba.labx7.consumerdemo.feign.UserFeignClient;
+import cn.iocoder.springcloudalibaba.labx7.api.UserService;
 import cn.iocoder.springcloudalibaba.labx7.dto.UserDTO;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user01")
-public class User01Controller {
+@RequestMapping("/user04")
+public class User04Controller {
 
-    @Autowired
-    private UserFeignClient userFeignClient;
+    @Reference(version = "1.0.0", protocol = "dubbo")
+    private UserService userService;
 
     @GetMapping("/get")
     public UserDTO get(@RequestParam("id") Integer id) {
-        return userFeignClient.get(id);
+        return userService.get(id);
     }
 
 }
