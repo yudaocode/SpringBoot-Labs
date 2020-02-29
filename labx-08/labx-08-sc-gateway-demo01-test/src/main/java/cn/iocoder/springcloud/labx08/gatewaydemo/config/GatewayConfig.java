84a92f94c3fc05a2d5970dd2e6e71ee50e21aa87
YunaiV <>
2020-03-01 01:36:12 +0800

    增加 spring cloud gateway 网关的示例

--- labx-08/labx-08-sc-gateway-demo01-test/src/main/java/cn/iocoder/springcloud/labx08/gatewaydemo/config/GatewayConfig.java
+++ labx-08/labx-08-sc-gateway-demo01-test/src/main/java/cn/iocoder/springcloud/labx08/gatewaydemo/config/GatewayConfig.java
@@ -4,11 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import reactor.core.publisher.Mono;
 
-@Configuration
+//@Configuration
 public class GatewayConfig {
 
     private Logger logger = LoggerFactory.getLogger(GatewayConfig.class);
