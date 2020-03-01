74fc78b0e6d82e1c22824af46cb13e21111958fb
YunaiV <>
2020-03-01 17:39:12 +0800

    增加 spring cloud apollo 示例

---
+++ labx-09/labx-09-sc-apollo-demo-jasypt/src/test/java/cn/iocoder/springcloud/labx09/apollodemo/JasyptTest.java
@@ -0,0 +1,40 @@
+package cn.iocoder.springcloud.labx09.apollodemo;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JasyptTest {
+
+    @Autowired
+    private StringEncryptor encryptor;
+
+    @Test
+    public void encode() {
+        // 第一个加密
+        String password = "woshimima";
+        System.out.println(encryptor.encrypt(password));
+
+        // 第二个加密
+        password = "bushimima";
+        System.out.println(encryptor.encrypt(password));
+    }
+
+    @Value("${xxx-password:}")
+    private String xxxPassword;
+
+    @Test
+    public void print() {
+        System.out.println(xxxPassword);
+    }
+
+//    @Value("${jasypt.encryptor.password}")
+//    private String password;
+
+}
