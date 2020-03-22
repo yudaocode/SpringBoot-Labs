820a3ecc77388d0f354b4b1e72c3767d36953dfd
YunaiV <>
2020-03-22 20:44:49 +0800

    增加 spring cloud x skywalking 链路追踪

---
+++ labx-14/labx-14-sc-skywalking-feign/src/main/java/cn/iocoder/springcloud/labx14/springmvcdemo/FeignApplication.java
@@ -0,0 +1,15 @@
+package cn.iocoder.springcloud.labx14.springmvcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class FeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignApplication.class, args);
+    }
+
+}
