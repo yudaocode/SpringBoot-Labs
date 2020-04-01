644ef85f4b76316df5ca7f9496dc51e405c96f04
YunaiV <>
2020-04-01 19:25:13 +0800

    增加 spring boot x seata

---
+++ lab-52/lab-52-multiple-datasource/src/main/java/cn/iocoder/springboot/lab52/seatademo/MultipleDatasourceApplication.java
@@ -0,0 +1,13 @@
+package cn.iocoder.springboot.lab52.seatademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleDatasourceApplication.class, args);
+    }
+
+}
