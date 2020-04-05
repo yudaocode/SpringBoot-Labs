5e96f4f7d168d8936f16a2367ca16b2f2d57ea38
YunaiV <>
2020-04-05 23:13:58 +0800

    增加 spring cloud alibaba seata 示例

---
+++ labx-17/labx-17-sca-seata-at-dubbo-demo/lab-53-seata-at-dubbo-demo-order-service/src/main/java/cn/iocoder/springboot/lab53/orderservice/controller/OrderController.java
@@ -0,0 +1,29 @@
+package cn.iocoder.springboot.lab53.orderservice.controller;
+
+import cn.iocoder.springboot.lab53.orderservice.api.OrderService;
+import org.apache.dubbo.config.annotation.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    private Logger logger = LoggerFactory.getLogger(OrderController.class);
+
+    @Reference
+    private OrderService orderService;
+
+    @PostMapping("/create")
+    public Integer createOrder(@RequestParam("userId") Long userId,
+                               @RequestParam("productId") Long productId,
+                               @RequestParam("price") Integer price) throws Exception {
+        logger.info("[createOrder] 收到下单请求,用户:{}, 商品:{}, 价格:{}", userId, productId, price);
+        return orderService.createOrder(userId, productId, price);
+    }
+
+}
---
+++ labx-17/labx-17-sca-seata-at-dubbo-demo/labx-17-sca-seata-at-dubbo-demo-account-service-api/src/main/java/cn/iocoder/springcloudalibaba/labx17/accountservice/api/AccountService.java
@@ -0,0 +1,17 @@
+package cn.iocoder.springcloudalibaba.labx17.accountservice.api;
+
+/**
+ * 账户 Service
+ */
+public interface AccountService {
+
+    /**
+     * 扣除余额
+     *
+     * @param userId 用户编号
+     * @param price  扣减金额
+     * @throws Exception 失败时抛出异常
+     */
+    void reduceBalance(Long userId, Integer price) throws Exception;
+
+}
