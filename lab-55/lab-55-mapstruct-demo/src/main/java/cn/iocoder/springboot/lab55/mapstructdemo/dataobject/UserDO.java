c2bb1cb54e0e62879cbefd633094e9d57f07bf08
YunaiV <>
2020-04-18 07:50:10 +0800

    增加 mapstruct 示例

---
+++ lab-55/lab-55-mapstruct-demo/src/main/java/cn/iocoder/springboot/lab55/mapstructdemo/dataobject/UserDO.java
@@ -0,0 +1,41 @@
+package cn.iocoder.springboot.lab55.mapstructdemo.dataobject;
+
+/**
+ * 用户 DO
+ */
+public class UserDO {
+
+    /** 用户编号 **/
+    private Integer id;
+    /** 用户名 **/
+    private String username;
+    /** 密码 **/
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public UserDO setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UserDO setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserDO setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+}
