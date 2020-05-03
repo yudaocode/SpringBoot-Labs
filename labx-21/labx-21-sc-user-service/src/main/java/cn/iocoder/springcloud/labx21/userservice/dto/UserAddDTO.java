fd35d92e00f756d38ffcbf2d17a268d82a31c131
YunaiV <>
2020-05-03 19:09:41 +0800

    增加 Zuul 示例

---
+++ labx-21/labx-21-sc-user-service/src/main/java/cn/iocoder/springcloud/labx21/userservice/dto/UserAddDTO.java
@@ -0,0 +1,37 @@
+package cn.iocoder.springcloud.labx21.userservice.dto;
+
+import java.io.Serializable;
+
+/**
+ * 用户添加 DTO
+ */
+public class UserAddDTO implements Serializable {
+
+    /**
+     * 昵称
+     */
+    private String name;
+    /**
+     * 性别
+     */
+    private Integer gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public UserAddDTO setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public UserAddDTO setGender(Integer gender) {
+        this.gender = gender;
+        return this;
+    }
+
+}
