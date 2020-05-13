8aeb88fdcb49a03c81bcb50770f2e50a21afb308
YunaiV <>
2020-05-13 08:46:15 +0800

    增加 Hystrix 入门示例（Hystrix Turbine）

---
+++ labx-23/labx-23-scn-hystrix-dashboard-turbine/src/main/java/cn/iocoder/springcloud/labx23/hystrixdemo/HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package cn.iocoder.springcloud.labx23.hystrixdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableTurbine
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+
+}
---
+++ labx-23/labx-23-scn-hystrix-demo01-cluster/src/main/java/cn/iocoder/springcloud/labx23/hystrixdemo/controller/DemoController.java
@@ -0,0 +1,35 @@
+package cn.iocoder.springcloud.labx23.hystrixdemo.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/get_user")
+    @HystrixCommand(fallbackMethod = "getUserFallback")
+    public String getUser(@RequestParam("id") Integer id) {
+        logger.info("[getUser][准备调用 user-service 获取用户({})详情]", id);
+        return restTemplate.getForEntity("http://127.0.0.1:18080/user/get?id=" + id, String.class).getBody();
+    }
+
+    public String getUserFallback(Integer id, Throwable throwable) {
+        logger.info("[getUserFallback][id({}) exception({})]", id, ExceptionUtils.getRootCauseMessage(throwable));
+        return "mock:User:" + id;
+    }
+
+}
