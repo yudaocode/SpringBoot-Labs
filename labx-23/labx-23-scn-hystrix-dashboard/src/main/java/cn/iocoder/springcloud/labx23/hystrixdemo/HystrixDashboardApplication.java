9af1942c269497f3cf4e6ad286a7a40e704bcd28
YunaiV <>
2020-05-13 08:02:19 +0800

    增加 Hystrix 入门示例（Hystrix Dashboard）

---
+++ labx-23/labx-23-scn-hystrix-dashboard/src/main/java/cn/iocoder/springcloud/labx23/hystrixdemo/HystrixDashboardApplication.java
@@ -0,0 +1,15 @@
+package cn.iocoder.springcloud.labx23.hystrixdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+
+}
