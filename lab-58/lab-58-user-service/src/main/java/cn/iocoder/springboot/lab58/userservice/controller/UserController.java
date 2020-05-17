653a27a9a72d686c853536c6817a4b230838e2a6
YunaiV <>
2020-05-17 18:06:20 +0800

    增加 spring boot + feign 示例

---
+++ lab-58/lab-58-user-service/src/main/java/cn/iocoder/springboot/lab58/userservice/controller/UserController.java
@@ -0,0 +1,33 @@
+package cn.iocoder.springboot.lab58.userservice.controller;
+
+import cn.iocoder.springboot.lab58.userservice.response.UserResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/get")
+    public UserResponse get(@RequestParam("id") Integer id) {
+        return new UserResponse().setId(id)
+                .setName("昵称：" + id).setGender(id % 2 == 0 ? 1 : 2);
+    }
+
+    @GetMapping("/list")
+    public List<UserResponse> list(@RequestParam("name") String name,
+                                   @RequestParam("gender") Integer gender) {
+        List<UserResponse> users = new ArrayList<>();
+        for (int id = 1; id <= 3; id++) {
+            users.add(new UserResponse().setId(id)
+                .setName(name).setGender(gender));
+        }
+        return users;
+    }
+
+}
