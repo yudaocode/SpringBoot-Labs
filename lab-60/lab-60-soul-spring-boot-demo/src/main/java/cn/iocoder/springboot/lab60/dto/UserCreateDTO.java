bec9ffb885d0a82219686aa4ae536ff467404a9d
YunaiV <>
2020-05-23 10:30:17 +0800

    soul 示例项目

---
+++ lab-60/lab-60-soul-spring-boot-demo/src/main/java/cn/iocoder/springboot/lab60/dto/UserCreateDTO.java
@@ -0,0 +1,34 @@
+package cn.iocoder.springboot.lab60.dto;
+
+/**
+ * 用户创建 DTO
+ */
+public class UserCreateDTO {
+
+    /**
+     * 昵称
+     */
+    private String nickname;
+    /**
+     * 性别
+     */
+    private Integer gender;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public UserCreateDTO setNickname(String nickname) {
+        this.nickname = nickname;
+        return this;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public UserCreateDTO setGender(Integer gender) {
+        this.gender = gender;
+        return this;
+    }
+}
