13aa1df2c872bcbc1b9b38432a81110a730ca108
YunaiV <>
2020-06-09 09:01:57 +0800

    增加 Spring Cloud Zookeeper 配置中心入门

---
+++ labx-26/labx-26-sc-zookeeper-config-auto-refresh/src/main/java/cn/iocoder/springcloud/labx26/zookeeperdemo/config/OrderProperties.java
@@ -0,0 +1,52 @@
+package cn.iocoder.springcloud.labx26.zookeeperdemo.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "order")
+public class OrderProperties {
+
+    /**
+     * 订单支付超时时长，单位：秒。
+     */
+    private Integer payTimeoutSeconds;
+
+    /**
+     * 订单创建频率，单位：秒
+     */
+    private Integer createFrequencySeconds;
+
+//    /**
+//     * 配置描述
+//     */
+//    private String desc;
+
+    public Integer getPayTimeoutSeconds() {
+        return payTimeoutSeconds;
+    }
+
+    public OrderProperties setPayTimeoutSeconds(Integer payTimeoutSeconds) {
+        this.payTimeoutSeconds = payTimeoutSeconds;
+        return this;
+    }
+
+    public Integer getCreateFrequencySeconds() {
+        return createFrequencySeconds;
+    }
+
+    public OrderProperties setCreateFrequencySeconds(Integer createFrequencySeconds) {
+        this.createFrequencySeconds = createFrequencySeconds;
+        return this;
+    }
+
+//    public String getDesc() {
+//        return desc;
+//    }
+//
+//    public OrderProperties setDesc(String desc) {
+//        this.desc = desc;
+//        return this;
+//    }
+
+}
