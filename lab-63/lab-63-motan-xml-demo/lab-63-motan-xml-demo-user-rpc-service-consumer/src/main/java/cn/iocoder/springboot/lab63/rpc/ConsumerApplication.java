3f99bc4e9774ba126627731f67916a7588771601
YunaiV <>
2020-06-10 23:45:23 +0800

    增加 Motan 入门

--- lab-63/lab-63-motan-xml-demo/lab-63-motan-xml-demo-user-rpc-service-consumer/src/main/java/cn/iocoder/springboot/lab63/rpc/ConsumerApplication.java
+++ lab-63/lab-63-motan-xml-demo/lab-63-motan-xml-demo-user-rpc-service-consumer/src/main/java/cn/iocoder/springboot/lab63/rpc/ConsumerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:sofarpc.xml")
+@ImportResource("classpath:motan.xml")
 public class ConsumerApplication {
 
     public static void main(String[] args) {
--- lab-63/lab-63-motan-xml-demo/lab-63-motan-xml-demo-user-rpc-service-provider/src/main/java/cn/iocoder/springboot/lab63/rpc/ProviderApplication.java
+++ lab-63/lab-63-motan-xml-demo/lab-63-motan-xml-demo-user-rpc-service-provider/src/main/java/cn/iocoder/springboot/lab63/rpc/ProviderApplication.java
@@ -1,16 +1,20 @@
 package cn.iocoder.springboot.lab63.rpc;
 
+import com.weibo.api.motan.common.MotanConstants;
+import com.weibo.api.motan.util.MotanSwitcherUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:sofarpc.xml")
+@ImportResource("classpath:motan.xml")
 public class ProviderApplication {
 
     public static void main(String[] args) {
+
         // 启动 Spring Boot 应用
         SpringApplication.run(ProviderApplication.class, args);
+        MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
     }
 
 }
