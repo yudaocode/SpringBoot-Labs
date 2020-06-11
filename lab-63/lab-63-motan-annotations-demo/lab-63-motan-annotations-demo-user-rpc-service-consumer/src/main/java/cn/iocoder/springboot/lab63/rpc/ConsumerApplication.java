7efaa26c65fc1bd64502cffad4d5512f30ab532a
YunaiV <>
2020-06-11 09:29:33 +0800

    增加 Motan 入门

--- lab-63/lab-63-motan-annotations-demo/lab-63-motan-annotations-demo-user-rpc-service-consumer/src/main/java/cn/iocoder/springboot/lab63/rpc/ConsumerApplication.java
+++ lab-63/lab-63-motan-annotations-demo/lab-63-motan-annotations-demo-user-rpc-service-consumer/src/main/java/cn/iocoder/springboot/lab63/rpc/ConsumerApplication.java
@@ -1,20 +1,16 @@
 package cn.iocoder.springboot.lab63.rpc;
 
-import com.weibo.api.motan.common.MotanConstants;
-import com.weibo.api.motan.util.MotanSwitcherUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource("classpath:motan.xml")
-public class ProviderApplication {
+public class ConsumerApplication {
 
     public static void main(String[] args) {
         // 启动 Spring Boot 应用
-        SpringApplication.run(ProviderApplication.class, args);
-        // 设置 Motan 开启对外服务
-        MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
+        SpringApplication.run(ConsumerApplication.class, args);;
     }
 
 }
