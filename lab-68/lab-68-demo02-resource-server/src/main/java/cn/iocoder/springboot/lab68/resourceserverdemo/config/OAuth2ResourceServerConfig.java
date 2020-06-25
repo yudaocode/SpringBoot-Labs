c39271019c2a3fee7abe04f0731ebd4c280f9e0b
YunaiV <>
2020-06-25 19:44:58 +0800

    增加 OAuth2 授权码模式

--- lab-68/lab-68-demo02-resource-server/src/main/java/cn/iocoder/springboot/lab68/resourceserverdemo/config/OAuth2ResourceServerConfig.java
+++ lab-68/lab-68-demo02-resource-server/src/main/java/cn/iocoder/springboot/lab68/resourceserverdemo/config/OAuth2ResourceServerConfig.java
@@ -17,6 +17,8 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
         http.authorizeRequests()
             // 设置 /login 无需权限访问
             .antMatchers("/login").permitAll()
+            /// 设置 /callback 无需权限访问
+            .antMatchers("/callback").permitAll()
             // 设置其它请求，需要认证后访问
             .anyRequest().authenticated()
             ;
