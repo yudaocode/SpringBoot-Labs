17a49ddae2b1ca0b8a3533d04b1bd1ba89db4cde
YunaiV <>
2020-11-21 16:37:47 +0800

    使用 JApiDocs 生成接口

---
+++ lab-24/lab-24-apidoc-japidocs/src/main/java/cn/iocoder/springboot/lab24/vo/UserRespVO.java
@@ -0,0 +1,21 @@
+package cn.iocoder.springboot.lab24.vo;
+
+/**
+ * 用户响应 VO
+ */
+public class UserRespVO {
+
+    /**
+     * 用户编号
+     */
+    private Integer id;
+    /**
+     * 昵称
+     */
+    private String nickname;
+    /**
+     * 年龄
+     */
+    private Integer age;
+
+}
