5be6fe347b92e579cdf619b1d50cb3f3e1b0f202
YunaiV <zhijiantianya@gmail.com>
2021-12-04 09:02:21 +0800

    增加 mybatis plus 多租户的示例

---
+++ lab-12-mybatis/lab-12-mybatis-plus-tenant/src/main/java/cn/iocoder/springboot/lab12/mybatis/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package cn.iocoder.springboot.lab12.mybatis.mapper;
+
+import cn.iocoder.springboot.lab12.mybatis.dataobject.UserDO;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface UserMapper extends BaseMapper<UserDO> {
+
+    default UserDO selectByUsername(@Param("username") String username) {
+        return selectOne(new QueryWrapper<UserDO>().eq("username", username));
+    }
+
+    List<UserDO> selectByIds(@Param("ids") Collection<Integer> ids);
+
+    default IPage<UserDO> selectPageByCreateTime(IPage<UserDO> page, @Param("createTime") Date createTime) {
+        return selectPage(page,
+                new QueryWrapper<UserDO>().gt("create_time", createTime)
+//                new QueryWrapper<UserDO>().like("username", "46683d9d")
+        );
+    }
+
+}
---
+++ lab-12-mybatis/lab-12-mybatis-plus-tenant/src/test/java/cn/iocoder/springboot/lab12/mybatis/mapper/UserMapperTest.java
@@ -0,0 +1,67 @@
+package cn.iocoder.springboot.lab12.mybatis.mapper;
+
+import cn.iocoder.springboot.lab12.mybatis.Application;
+import cn.iocoder.springboot.lab12.mybatis.dataobject.UserDO;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class UserMapperTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testInsert() {
+        UserDO user = new UserDO().setUsername(UUID.randomUUID().toString())
+                .setPassword("nicai").setCreateTime(new Date())
+                .setDeleted(0); // 一般情况下，是否删除，可以全局枚举下。
+        userMapper.insert(user);
+    }
+
+    @Test
+    public void testUpdateById() {
+        UserDO updateUser = new UserDO().setId(1)
+                .setPassword("wobucai");
+        userMapper.updateById(updateUser);
+    }
+
+    @Test
+    public void testDeleteById() {
+        userMapper.deleteById(2);
+    }
+
+    @Test
+    public void testSelectById() {
+        userMapper.selectById(1);
+    }
+
+    @Test
+    public void testSelectByUsername() {
+        UserDO userDO = userMapper.selectByUsername("yunai");
+        System.out.println(userDO);
+    }
+
+    @Test
+    public void testSelectByIds() {
+        List<UserDO> users = userMapper.selectByIds(Arrays.asList(1, 3));
+        System.out.println("users：" + users.size());
+    }
+
+    @Test
+    public void testSelectPageByCreateTime() {
+        IPage<UserDO> page = new Page<>(1, 10);
+        Date createTime = new Date(2018 - 1990, Calendar.FEBRUARY, 24); // 临时 Demo ，实际不建议这么写
+        page = userMapper.selectPageByCreateTime(page, createTime);
+        System.out.println("users：" + page.getRecords().size());
+    }
+
+}
